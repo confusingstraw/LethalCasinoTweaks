bb7a6484704fc4f6ab84f3066aa5e4bef7e93e2b
Rob Foley <rob.foley.jr@gmail.com>
2025-02-22 16:05:41 -0500

    refactor patch into separate file

--- LethalCasinoTweaks/LethalCasinoTweaks/Patches/BlackjackPatch.cs
+++ LethalCasinoTweaks/LethalCasinoTweaks/Patches/BlackjackPatch.cs
@@ -1,10 +1,5 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Emit;
 using HarmonyLib;
 using LethalCasino.Custom;
-using UnityEngine;
 
 namespace LethalCasinoTweaks.Patches;
 
@@ -87,58 +82,5 @@ public class BlackjackPatch
     private static bool ShouldCreateDeck(Blackjack instance)
     {
         return instance.deck == null || instance.deck.Count == 0;
-    }
-}
-
-[HarmonyPatch(typeof(Blackjack), "StartGameClientRpc")]
-public static class BlackjackStartGameClientRpcPatch
-{
-    private static readonly MethodInfo MPlayOneShot =
-        AccessTools.Method(typeof(AudioSource), "PlayOneShot", [typeof(AudioClip), typeof(float)]);
-
-    private static readonly FieldInfo FAudioSource =
-        AccessTools.Field(typeof(Blackjack), "audioSource");
-
-    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-    {
-        LethalCasinoTweaks.Logger.LogDebug("Generating reverse patch code");
-        var list = instructions.ToList();
-        
-        var startIndex = -1;
-        var endIndex = -1;
-            
-        for (var i = 0; i < list.Count; i++)
-        {
-            if (startIndex == -1)
-            {
-                var nextIndex = i + 1;
-                if (
-                    list[i].opcode == OpCodes.Ldarg_0 &&
-                    nextIndex < list.Count &&
-                    list[nextIndex].opcode == OpCodes.Ldfld &&
-                    (FieldInfo)list[nextIndex].operand == FAudioSource)
-                {
-                    startIndex = i;
-                }
-            } else 
-            {
-                if (list[i].opcode == OpCodes.Callvirt && (MethodInfo)list[i].operand == MPlayOneShot)
-                {
-                    endIndex = i;
-                }
-            }
-        }
-            
-        if (startIndex > -1 && endIndex > -1)
-        {
-            LethalCasinoTweaks.Logger.LogDebug($"Found instructions to patch, removing: [{startIndex}, {endIndex}]");
-            list.RemoveRange(startIndex, (endIndex - startIndex) + 1);
-        }
-        else
-        {
-            LethalCasinoTweaks.Logger.LogWarning($"Failed to find audioSource call to patch: [{startIndex}, {endIndex}]");
-        }
-            
-        return list.AsEnumerable();
     }
 }
---
+++ LethalCasinoTweaks/LethalCasinoTweaks/Patches/BlackjackStartGameClientRpcPatch.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using LethalCasino.Custom;
+using UnityEngine;
+
+namespace LethalCasinoTweaks.Patches;
+
+/**
+ * Patches the `StartGameClientRpc` method so that it no longer plays the shuffle sound.
+ */
+[HarmonyPatch(typeof(Blackjack), "StartGameClientRpc")]
+public static class BlackjackStartGameClientRpcPatch
+{
+    private static readonly MethodInfo MPlayOneShot =
+        AccessTools.Method(typeof(AudioSource), "PlayOneShot", [typeof(AudioClip), typeof(float)]);
+
+    private static readonly FieldInfo FAudioSource =
+        AccessTools.Field(typeof(Blackjack), "audioSource");
+
+    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        LethalCasinoTweaks.Logger.LogDebug("Generating reverse patch code");
+        var list = instructions.ToList();
+        
+        var startIndex = -1;
+        var endIndex = -1;
+            
+        for (var i = 0; i < list.Count; i++)
+        {
+            if (startIndex == -1)
+            {
+                var nextIndex = i + 1;
+                if (
+                    list[i].opcode == OpCodes.Ldarg_0 &&
+                    nextIndex < list.Count &&
+                    list[nextIndex].opcode == OpCodes.Ldfld &&
+                    (FieldInfo)list[nextIndex].operand == FAudioSource)
+                {
+                    startIndex = i;
+                }
+            } else 
+            {
+                if (list[i].opcode == OpCodes.Callvirt && (MethodInfo)list[i].operand == MPlayOneShot)
+                {
+                    endIndex = i;
+                }
+            }
+        }
+            
+        if (startIndex > -1 && endIndex > -1)
+        {
+            LethalCasinoTweaks.Logger.LogDebug($"Found instructions to patch, removing: [{startIndex}, {endIndex}]");
+            list.RemoveRange(startIndex, (endIndex - startIndex) + 1);
+        }
+        else
+        {
+            LethalCasinoTweaks.Logger.LogWarning($"Failed to find audioSource call to patch: [{startIndex}, {endIndex}]");
+        }
+            
+        return list.AsEnumerable();
+    }
+}
