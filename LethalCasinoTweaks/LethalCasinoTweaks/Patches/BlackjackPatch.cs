21138463bce5094cb48f815e6907f3ca2d202cb4
Rob Foley <rob.foley.jr@gmail.com>
2025-02-21 23:32:36 -0500

    only play shuffle sound when actually shuffling

--- LethalCasinoTweaks/LethalCasinoTweaks/Patches/BlackjackPatch.cs
+++ LethalCasinoTweaks/LethalCasinoTweaks/Patches/BlackjackPatch.cs
@@ -1,6 +1,10 @@
-using BepInEx.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using HarmonyLib;
 using LethalCasino.Custom;
+using UnityEngine;
 
 namespace LethalCasinoTweaks.Patches;
 
@@ -13,39 +17,45 @@ public class BlackjackPatch
      * `CreateDeck` is what actually restores the playing deck
      * to its initial state. We hook into it and only allow
      * the call to go through if we deem it necessary.
+     *
+     * The patch state is used to track whether we created a deck or not.
      */
     [HarmonyPatch("CreateDeck")]
     [HarmonyPrefix]
-    private static bool CreateDeckPrefix(Blackjack __instance)
+    private static bool CreateDeckPrefix(Blackjack __instance, out bool __state)
     {
         if (!ShouldCreateDeck(__instance))
         {
             LethalCasinoTweaks.Logger.LogDebug("Skipping create deck while non-empty");
+            __state = false;
             return false;
         }
 
         LethalCasinoTweaks.Logger.LogDebug("Creating/shuffling deck");
+        __state = true;
         return true;
     }
     
-    /// End CreateDeck Patches
-    
-    /// Start ShuffleDeck Patches
-
     /**
-     * `CreateDeck` is what actually restores the playing deck
-     * to its initial state. We hook into it and only allow
-     * the call to go through if we deem it necessary.
+     * Here we check if the deck actually got created/shuffled and play the
+     * sound across all clients if it did.
      */
-    [HarmonyPatch("ShuffleDeck")]
+    [HarmonyPatch("CreateDeck")]
     [HarmonyPostfix]
-    private static void ShuffleDeckPostfix(Blackjack __instance)
+    private static void CreateDeckPostfix(Blackjack __instance, bool __state)
     {
-        LethalCasinoTweaks.Logger.LogDebug("Playing shuffle sound for all clients");
-        __instance.PlaySoundClientRpc("ShuffleDeck", 1f);
+        if (__state)
+        {
+            LethalCasinoTweaks.Logger.LogDebug("Playing shuffle sound for all clients");
+            __instance.PlaySoundClientRpc("ShuffleDeck", 1f);   
+        }
+        else
+        {
+            LethalCasinoTweaks.Logger.LogDebug("Didn't create a deck, skipping shuffle sound");
+        }
     }
     
-    /// End ShuffleDeck Patches
+    /// End CreateDeck Patches
     
     /// Start DealCard Patches
     
@@ -65,36 +75,9 @@ public class BlackjackPatch
             __instance.CreateDeck();
         }
     }
-    
+
     /// End DealCard Patches
     
-    /// Start StartGameClientRpc Patches
-
-    /**
-     * Mutes the audio source before calling the method, preventing it
-     * from playing the shuffle sound.
-     */
-    [HarmonyPatch("StartGameClientRpc")]
-    [HarmonyPrefix]
-    private static void StartGameClientRpcPrefix(Blackjack __instance)
-    {
-        LethalCasinoTweaks.Logger.LogDebug("Muting audio source to prevent initial shuffle sound on game start");
-        __instance.audioSource.mute = true;
-    }
-
-    /**
-     * Unmute the audio source after the call regardless of what happens. 
-     */
-    [HarmonyPatch("StartGameClientRpc")]
-    [HarmonyFinalizer]
-    private static void StartGameClientRpcFinalizer(Blackjack __instance)
-    {
-        LethalCasinoTweaks.Logger.LogDebug("Un-muting audio source after after game start");
-        __instance.audioSource.mute = false;
-    }
-    
-    /// End StartGameClientRpc Patches
-
     /// Utilities
 
     /**
@@ -104,5 +87,58 @@ public class BlackjackPatch
     private static bool ShouldCreateDeck(Blackjack instance)
     {
         return instance.deck == null || instance.deck.Count == 0;
+    }
+}
+
+[HarmonyPatch(typeof(Blackjack), "StartGameClientRpc")]
+public static class BlackjackStartGameClientRpcPatch
+{
+    private static readonly MethodInfo MPlayOneShot =
+        AccessTools.Method(typeof(AudioSource), "PlayOneShot", [typeof(AudioClip), typeof(float)]);
+
+    private static readonly FieldInfo FAudioSource =
+        AccessTools.Field(typeof(Blackjack), "audioSource");
+
+    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        LethalCasinoTweaks.Logger.LogDebug("Generating reverse patch code");
+        var list = instructions.ToList();
+        
+        var startIndex = -1;
+        var endIndex = -1;
+            
+        for (var i = 0; i < list.Count; i++)
+        {
+            if (startIndex == -1)
+            {
+                var nextIndex = i + 1;
+                if (
+                    list[i].opcode == OpCodes.Ldarg_0 &&
+                    nextIndex < list.Count &&
+                    list[nextIndex].opcode == OpCodes.Ldfld &&
+                    (FieldInfo)list[nextIndex].operand == FAudioSource)
+                {
+                    startIndex = i;
+                }
+            } else 
+            {
+                if (list[i].opcode == OpCodes.Callvirt && (MethodInfo)list[i].operand == MPlayOneShot)
+                {
+                    endIndex = i;
+                }
+            }
+        }
+            
+        if (startIndex > -1 && endIndex > -1)
+        {
+            LethalCasinoTweaks.Logger.LogDebug($"Found instructions to patch, removing: [{startIndex}, {endIndex}]");
+            list.RemoveRange(startIndex, (endIndex - startIndex) + 1);
+        }
+        else
+        {
+            LethalCasinoTweaks.Logger.LogWarning($"Failed to find audioSource call to patch: [{startIndex}, {endIndex}]");
+        }
+            
+        return list.AsEnumerable();
     }
 }
